80ed1552fda873c03131dae837f54ab6690c4d70
João Vitor Victorio <joaovitorvictorio@gmail.com>
2024-07-10 23:43:26 -0300

    Project update

--- script.js
+++ script.js
@@ -13,21 +13,36 @@ const formatador = (data) => {
 }
 
 const atividade = {
-    nome: "Café da manhã",
-    data: new Date("2024-07-03 08:00"),
+    nome: "Treino de Ombro",
+    data: new Date("2024-07-01 19:00"),
     finalizada: true
 }
 
 let atividades = [
     atividade,
     {
-        nome: 'Academia em grupo',
-        data: new Date("2024-07-08 12:00"),
+        nome: 'Treino de Costas',
+        data: new Date("2024-07-02 19:00"),
         finalizada: true
     },
     {
-        nome: 'Almoço',
-        data: new Date("2024-07-08 13:00"),
+        nome: 'Treino de Perna',
+        data: new Date("2024-07-03 19:00"),
+        finalizada: true
+    },
+    {
+        nome: 'Treino de Biceps',
+        data: new Date("2024-07-04 19:00"),
+        finalizada: true
+    },
+    {
+        nome: 'Treino de Triceps',
+        data: new Date("2024-07-05 19:00"),
+        finalizada: true
+    },
+    {
+        nome: 'Treino de Peito',
+        data: new Date("2024-07-06 19:00"),
         finalizada: true
     }
 ]
@@ -49,12 +64,12 @@ const criarItemAtividade = (atividade) => {
     const formatar = formatador(atividade.data)
 
     return `
-        <div>
+        <div class="card-bg">
             ${input}
             <span>${atividade.nome},</span>
             <time>${formatar.dia.semana.longo}, 
             dia ${formatar.dia.numerico} de 
-            ${formatar.mes}</time>
+            ${formatar.mes} às ${formatar.hora}h</time>
         </div>
     `
 }
@@ -131,7 +146,7 @@ criarDiasSelecao()
 const criarHorasSelecao = () => {
     let horasDisponiveis = ''
 
-    for (let index = 6; index < 23; index++) {
+    for (let index = 6; index <= 23; index++) {
         const hora = String(index).padStart(2, '0')
         horasDisponiveis += `<option value="${hora}:00">${hora}:00</option>`
         horasDisponiveis += `<option value="${hora}:30">${hora}:30</option>`
