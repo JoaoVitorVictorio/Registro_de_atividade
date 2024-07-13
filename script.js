2ad09dccba592403c0ce7a29016fb2e662bde797
João Vitor Victorio <joaovitorvictorio@gmail.com>
2024-07-13 00:18:31 -0300

    The application developed during Rocketseat's NLW journey on the HTML, CSS and JavaScript track has been completed.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Função para adicionar dias a uma data
+function adicionarDias(data, dias) {
+    let novaData = new Date(data);
+    novaData.setDate(novaData.getDate() + dias);
+    return novaData;
+}
+
+// Data atual
+const dataAtual = new Date();
+
+// Função para formatar a data
 const formatador = (data) => {
     return {
         dia: {
@@ -13,38 +24,13 @@ const formatador = (data) => {
 }
 
 const atividade = {
-    nome: "Treino de Ombro",
-    data: new Date("2024-07-01 19:00"),
+    nome: "Atividade",
+    data: adicionarDias(dataAtual, 0),
     finalizada: true
 }
 
 let atividades = [
-    atividade,
-    {
-        nome: 'Treino de Costas',
-        data: new Date("2024-07-02 19:00"),
-        finalizada: true
-    },
-    {
-        nome: 'Treino de Perna',
-        data: new Date("2024-07-03 19:00"),
-        finalizada: true
-    },
-    {
-        nome: 'Treino de Biceps',
-        data: new Date("2024-07-04 19:00"),
-        finalizada: true
-    },
-    {
-        nome: 'Treino de Triceps',
-        data: new Date("2024-07-05 19:00"),
-        finalizada: true
-    },
-    {
-        nome: 'Treino de Peito',
-        data: new Date("2024-07-06 19:00"),
-        finalizada: true
-    }
+    
 ]
 
 const criarItemAtividade = (atividade) => {
@@ -64,13 +50,31 @@ const criarItemAtividade = (atividade) => {
     const formatar = formatador(atividade.data)
 
     return `
-        <div class="card-bg">
-            ${input}
-            <span>${atividade.nome},</span>
-            <time>${formatar.dia.semana.longo}, 
-            dia ${formatar.dia.numerico} de 
-            ${formatar.mes} às ${formatar.hora}h</time>
-        </div>
+    <div class="card-bg">
+      ${input}
+
+      <div>
+        <svg class="active" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M7.50008 10L9.16675 11.6667L12.5001 8.33335M18.3334 10C18.3334 14.6024 14.6025 18.3334 10.0001 18.3334C5.39771 18.3334 1.66675 14.6024 1.66675 10C1.66675 5.39765 5.39771 1.66669 10.0001 1.66669C14.6025 1.66669 18.3334 5.39765 18.3334 10Z" stroke="#BEF264" style="stroke:#BEF264;stroke:color(display-p3 0.7451 0.9490 0.3922);stroke-opacity:1;" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+
+      <svg class="inactive" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8.41664 1.81836C9.46249 1.61597 10.5374 1.61597 11.5833 1.81836M11.5833 18.1817C10.5374 18.3841 9.46249 18.3841 8.41664 18.1817M14.6741 3.10086C15.5587 3.70022 16.3197 4.46409 16.9158 5.35086M1.8183 11.5834C1.6159 10.5375 1.6159 9.46255 1.8183 8.4167M16.8991 14.6742C16.2998 15.5588 15.5359 16.3198 14.6491 16.9159M18.1816 8.4167C18.384 9.46255 18.384 10.5375 18.1816 11.5834M3.1008 5.32586C3.70016 4.44131 4.46403 3.68026 5.3508 3.0842M5.3258 16.8992C4.44124 16.2998 3.6802 15.536 3.08414 14.6492" stroke="#A1A1AA" style="stroke:#A1A1AA;stroke:color(display-p3 0.6314 0.6314 0.6667);stroke-opacity:1;" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+      
+      <span>${atividade.nome}</span>
+      </div>
+
+      <time class="short">
+      ${formatar.dia.semana.curto}.
+      ${formatar.dia.numerico} <br>
+      ${formatar.hora}
+      </time>
+      <time class="full">${formatar.dia.semana.longo}, 
+      dia ${formatar.dia.numerico}
+      de ${formatar.mes} 
+      às ${formatar.hora}h </time>
+    </div>
     `
 }
 
@@ -118,15 +122,7 @@ const salvarAtividade = (event) => {
 }
 
 const criarDiasSelecao = () => {
-    const dias = [
-        "2024-02-28",
-        "2024-02-29",
-        "2024-03-01",
-        "2024-03-02",
-        "2024-03-03",
-        "2024-03-04",
-        "2024-03-05",
-    ]
+    const dias = Array.from({length: 7}, (_, i) => adicionarDias(dataAtual, i).toISOString().split('T')[0])
 
     let diasSelecao = '';
 
@@ -174,3 +170,29 @@ const concluirAtividade = (event) => {
     atividade.finalizada = !atividade.finalizada
     atualizarListaDeAtividades()
 }
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        document.getElementById("location").innerText
+        document.getElementById("location").innerText = "Geolocalização não suportada.";
+    }
+}
+
+function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`)
+        .then(response => response.json())
+        .then(data => {
+            const address = data.address.city + ", " + data.address.state;
+            document.getElementById("location").innerText = address;
+        })
+        .catch(error => {
+            console.error('Erro ao obter endereço:', error);
+            document.getElementById("location").innerText = `Latitude: ${latitude}, Longitude: ${longitude}`;
+        });
+}
+
+window.onload = getLocation;
